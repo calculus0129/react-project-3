95a6d64c26646357cb6e77f1aded936dd394629f
Jaehyun Bhang <ryanb@Jaehyunui-MacBookPro.local>
2024-03-19 09:44:03 +0900

    feat: control all values of input from App

--- src/components/InputText.jsx
+++ src/components/InputText.jsx
@@ -1,5 +1,12 @@
-export default function InputText({title}) {
+export default function InputText({ title, index, handleValue, type = 'float' }) {
     return <><label>{title}</label>
-    <input type="text" onChange={(hitEvent)=>console.log(parseInt(hitEvent.target.value))}></input>
+        <input type="text" onChange={(hitEvent) =>
+            handleValue(
+                index,
+                type == 'float'
+                    ? parseFloat(hitEvent.target.value.replace(',','.'))
+                    : parseInt(hitEvent.target.value)
+            )
+        }></input>
     </>;
 }
