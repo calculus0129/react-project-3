2b7d755e2e28df70e55aa4303a19f6306b672fa4
calculus0129 <ryanbhang129@naver.com>
2024-03-20 16:31:51 +0900

    feat: revise <input> tag with useful attributes

--- src/components/InputText.jsx
+++ src/components/InputText.jsx
@@ -1,5 +1,5 @@
 export default function InputText({ title, name, handleValue, type = 'float' }) {
-    return <><label>{title}</label>
+    return <><label type="number" required>{title}</label>
         <input type="text" onChange={(hitEvent) =>
             handleValue(
                 name,
